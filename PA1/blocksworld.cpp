f721b54f56829ad4f3fc4be4a18c751328261b99
joyceluo04 <156722245+joyceluo04@users.noreply.github.com>
2025-09-27 17:27:24 -0500

    fixed the vectors into strings for each stack

--- PA1/blocksworld.cpp
+++ PA1/blocksworld.cpp
@@ -4,7 +4,28 @@
 #include <sstream>
 #include <vector>
 using namespace std;
-
+int num_stacks = 0;
+int num_blocks = 0;
+int num_moves = 0;
+string stack1;
+string stack2;
+string stack3;
+string stack4;
+string stack5;
+vector<string> allstacks;
+vector<State> outsuccessors;
+string goalstack1;
+string goalstack2;
+string goalstack3;
+string goalstack4;
+string goalstack5; 
+vector<string> allgoalstacks;
+class State {         //
+  public:             // Access specifier
+    int myNum;        // Attribute (int variable)
+    string myString;  // Attribute (string variable)
+    vector <string> configuration;
+};
 void blocksworld(const string &filename) {
     cout << "Problem # file: " << filename << endl;
 
@@ -15,20 +36,7 @@ void blocksworld(const string &filename) {
     }
 
     string line;
-    int linecount = 0;
-    int num_stacks = 0;
-    int num_blocks = 0;
-    int num_moves = 0;
-    vector<char> stack1;
-    vector<char> stack2;
-    vector<char> stack3;
-    vector<char> stack4;
-    vector<char> stack5;
-    vector<char> goalstack1;
-    vector<char> goalstack2;
-    vector<char> goalstack3;
-    vector<char> goalstack4;
-    vector<char> goalstack5;  
+    int linecount = 0; 
     while (getline(in, line)) { //simple parser to read
         if (linecount == 0){
             std::istringstream iss(line);
@@ -40,85 +48,85 @@ void blocksworld(const string &filename) {
         }
         //reading in the initial stacks for 3 columns
         if(linecount == 2 && num_stacks==3){
-            string s = line;
-            for (char c : s) if (!isspace((unsigned char)c)) stack1.push_back(c);
+            stack1 = line;
         }
         if(linecount == 3 && num_stacks==3){
-            string s = line;
-            for (char c : s) if (!isspace((unsigned char)c)) stack2.push_back(c);
+            stack2 = line;
         }
         if(linecount == 4 && num_stacks==3){
-            string s = line;
-            for (char c : s) if (!isspace((unsigned char)c)) stack3.push_back(c);
+            stack3 = line;
+            allstacks = {stack1, stack2, stack3};
         }
         //reading in the goal stacks for 3 columns
-        if(linecount == 5 && num_stacks==3){
-            string s = line;
-            for (char c : s) if (!isspace((unsigned char)c)) goalstack1.push_back(c);
-        }
         if(linecount == 6 && num_stacks==3){
-            string s = line;
-            for (char c : s) if (!isspace((unsigned char)c)) goalstack2.push_back(c);
+            goalstack1 = line;
         }
         if(linecount == 7 && num_stacks==3){
-            string s = line;
-            for (char c : s) if (!isspace((unsigned char)c)) goalstack3.push_back(c);
+            goalstack2 = line;
+        }
+        if(linecount == 8 && num_stacks==3){
+            goalstack3 = line;
+            allgoalstacks = {goalstack1, goalstack2, goalstack3};
         }
         //reading in the stacks for 5 columns
         if(linecount == 2 && num_stacks==5){
-            string s = line;
-            for (char c : s) if (!isspace((unsigned char)c)) stack1.push_back(c);
+            stack1 = line;
         }
         if(linecount == 3 && num_stacks==5){
-            string s = line;
-            for (char c : s) if (!isspace((unsigned char)c)) stack2.push_back(c);
+            stack2 = line;
         }
         if(linecount == 4 && num_stacks==5){
-            string s = line;
-            for (char c : s) if (!isspace((unsigned char)c)) stack3.push_back(c);
+            stack3 = line;
         }
         if(linecount == 5 && num_stacks==5){
-            string s = line;
-            for (char c : s) if (!isspace((unsigned char)c)) stack4.push_back(c);
+            stack4 = line;
         }
         if(linecount == 6 && num_stacks==5){
-            string s = line;
-            for (char c : s) if (!isspace((unsigned char)c)) stack5.push_back(c);
+            stack5 = line;
+            allstacks = {stack1, stack2, stack3, stack4, stack5};
         }
         //reading in the goal stacks for 5 columns
         if(linecount == 8 && num_stacks==5){
-            string s = line;
-            for (char c : s) if (!isspace((unsigned char)c)) goalstack1.push_back(c);
+            goalstack1 = line;
         }
         if(linecount == 9 && num_stacks==5){
-            string s = line;
-            for (char c : s) if (!isspace((unsigned char)c)) goalstack2.push_back(c);
+            goalstack2 = line;
         }
         if(linecount == 10 && num_stacks==5){
-            string s = line;
-            for (char c : s) if (!isspace((unsigned char)c)) goalstack3.push_back(c);
+            goalstack3 = line;
         }
         if(linecount == 11 && num_stacks==5){
-            string s = line;
-            for (char c : s) if (!isspace((unsigned char)c)) goalstack4.push_back(c);
+            goalstack4 = line;
         }
         if(linecount == 12 && num_stacks==5){
-            string s = line;
-            for (char c : s) if (!isspace((unsigned char)c)) goalstack5.push_back(c);
+            goalstack5 = line;
+            allgoalstacks = {goalstack1, goalstack2, goalstack3, goalstack4, goalstack5};
         }
         linecount++;
     }
-    for (char i: stack1)
-    std::cout << i << ' ';
-    cout<<endl;
-    for (char i: stack2)
-    std::cout << i << ' ';
-    cout<<endl;
-    for (char i: stack3)
-    std::cout << i << ' ';
     cout << "File read completed." << endl;
+    //generate successors now
+//successor here
 }
+void successors(const vector<vector<char>> curr){
+    outsuccessors.clear();
+    for (int from = 0; from < num_stacks; ++from) {
+        if (curr[from].empty()) continue;            // nothing to move
 
+        const char topBlock = curr[from].back();     // block on top of 'from'
+
+        for (int to = 0; to < num_stacks; ++to) {
+            if (to == from) continue;                  // no self-move
+
+            State nextState;
+            nextState.allstacks = curr;        // copy current config
+            nextState.allstacks[from].pop_back();               // remove from source top
+            nextState.allstacks[to].push_back(topBlock);        // push onto dest top
+
+            outsuccessors.push_back({nextState});
+        }
+    }
+}
 // --- Program entry point ---
 int main(int argc, char *argv[]) {
 
